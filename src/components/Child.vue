c3e2d98464a0a6970b02becd9935b298ec4d16b2
zhangcan <394203959@qq.com>
2020-12-19 13:08:49 +0800

    [组件内部自己玩 EventBus]

---
+++ src/components/Child.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button>
+            child
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Child",
+        mounted() {
+            this.register();
+            this.dispatch();
+        },
+        methods: {
+            register() {
+                this.$on('aaaa', console.log)
+            },
+
+            dispatch() {
+                this.$emit('aaaa', '自己内部玩')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
