75c5c086f36b2eff6216cf01792aac5af9ebb898
zhangcan <394203959@qq.com>
2020-12-19 16:04:34 +0800

    [使用 off 后，每次 mounted，都只有一个 callback ]

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -17,6 +17,12 @@
             registerOnce() {
                 EventBus.$on('aa', console.log);
                 console.log('EventBus--methods',EventBus._events);
+
+                this.$once('hook:beforeDestroy', function () {
+                    EventBus.$off(); // 必须用 off，否则，之前注册的函数一直存在，即使组件被销毁了也在
+                    // EventBus.$off('aa');
+                    // EventBus.$off('aa', console.log);
+                })
             }
         },
         destroyed() {
